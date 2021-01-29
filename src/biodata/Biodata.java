427a4ed0e651cb206d632fe0ffe2099ebba1e4fd
AbdulMoizChishti <moaiz599@gmail.com>
2021-01-29 22:13:13 +0500

    BioData

---
+++ src/biodata/Biodata.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] arg) {
+        // TODO code application logic here
+    
+    int age=18;
+    String name ="Abdul Moiz Chishti";
+    String uni ="Sir Syed University";
+    String pn = "0332176";
+    String add = "R-144 sector ll-C/1 Latif Nagar North Karachi";
+    
+        System.out.println("*****************************");
+        System.out.println("\tBIODATA\n");
+        
+        System.out.println("Name:\t\t "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Address: "+add);
+        System.out.println("University: "+uni);
+        System.out.println("Phone Number: "+pn);
+    
+    
+    }
+}
